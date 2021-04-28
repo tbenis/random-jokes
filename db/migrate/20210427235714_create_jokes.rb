1979b813d408cd219f50e724a95f731f665771c1
Benis Tambe <will.jenifer@yahoo.com>
2021-04-27 21:33:09 -0400

    added fields to table

--- db/migrate/20210427235714_create_jokes.rb
+++ db/migrate/20210427235714_create_jokes.rb
@@ -2,6 +2,11 @@ class CreateJokes < ActiveRecord::Migration
   def change
     create_table :jokes do |t|
 
+      t.string :title
+      t.string :setup
+      t.string :punchline
+      t.integer :user_id
+
       t.timestamps null: false
     end
   end
