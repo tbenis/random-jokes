ad4dec7c227b5fecd90e4b916d4d383d67045e82
Benis Tambe <will.jenifer@yahoo.com>
2021-04-29 15:35:05 -0400

    removed.html and commented out unncessary routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,47 @@
 class UsersController < ApplicationController
 
   # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get '/users/new' do 
+    erb :'/users/new'
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+          erb :'users/new'
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
+  # # GET: /users
+  # get "/users" do
+  #   erb :"/users/index.html"
+  # end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
+  # # GET: /users/new
+  # get "/users/new" do
+  #   erb :"/users/new.html"
+  # end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
+  # # POST: /users
+  # post "/users" do
+  #   redirect "/users"
+  # end
+
+  # # GET: /users/5
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # end
+
+  # # GET: /users/5/edit
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
+
+  # # PATCH: /users/5
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
+
+  # # DELETE: /users/5/delete
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
   end
 
   # PATCH: /users/5
