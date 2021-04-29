dfb581ad666a1af89b0e6ba04446b111d2d05207
Benis Tambe <will.jenifer@yahoo.com>
2021-04-29 17:08:31 -0400

    loggin now works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   end
 
   post '/login' do 
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+        session[:id] = @user.id
         flash[:success] = "Successfully logged in!"
+        redirect "/users/#{session[:id]}"    
+    else
       flash[:error] = "Incorrect email or password. Try again."
+        redirect '/login'   
+    end
   end
 
-  delete '/logout' do 
+  get '/logout' do 
       session.clear
       flash[:success] = "You are now logged out."
       redirect "/"
