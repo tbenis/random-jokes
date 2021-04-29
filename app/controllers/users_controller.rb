849b9e37222c18d158b94fec8981a49d2fa15711
Benis Tambe <will.jenifer@yahoo.com>
2021-04-29 15:35:51 -0400

    now able to create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class UsersController < ApplicationController
     erb :'/users/new'
   end
 
+  post '/users' do 
+      @user = User.new(email: params[:email], password: params[:password])
+      
+      if @user.save
+          session[:id] = @user.id
+          redirect "/"
+      else 
           erb :'users/new'
+  end
   end
 
   # # GET: /users
