f896e8c929821c94c8abcb3187bc2d98cea36e83
Benis Tambe <will.jenifer@yahoo.com>
2021-04-29 16:52:20 -0400

    validating other user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: {with: /\A(?<name>[^@\s]+)@(?<domainname>([-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 5..100}, confirmation: true
     has_many :jokes
 end
